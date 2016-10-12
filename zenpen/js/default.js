339224dc1bb1ca3593e8f9f7e7a0f9c2a58d4597
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-10-12 09:40:49 -0500

    Update default.js

--- zenpen/js/default.js
+++ zenpen/js/default.js
@@ -7,10 +7,10 @@ Esta es una zona minimalista libre de distracciones para concentrarse en lo que 
 	Para comenzar, todo lo que necesitas es eliminar este texto (es en serio, solo resalta y elimina) y comienzar a escribir. \
 </p> \
 <p> \
-	Puedes usar <b>negritas</b>, <i>cursivas</i>, <b><i>ambas</i></b>, incluso and <a href="http://zenpen.io"> enlaces </a> con solo resaltar el text y elegir una opción en la caja que aparecerá.\
+	Puedes usar <b>negritas</b>, <i>cursivas</i>, <b><i>ambas</i></b>, incluso and <a href="http://zenpen.io"> enlaces </a> con solo resaltar el texto y elegir la opción adecuada en la caja que aparecerá.\
 </p>\
 <blockquote>\
-	Citar texto es igual de fácil\
+	Construir citas es igual de fácil\
 </blockquote>\
 <p>\
 	Para <i>preguntas</i> y <b>información open source</b>, Click en el símbolo de interrogación en la esquina inferior.\
